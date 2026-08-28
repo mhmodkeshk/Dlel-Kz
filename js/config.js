e660677b20941445bfe73dee98a455e9e90e980d
mhmodkeshk <mhmodkshk@gmail.com>
2026-08-28 21:55:12 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://kucmxumbpguwapjpibfm.supabase.co',
-  SUPABASE_ANON_KEY: 'ضع هنا الـ Publishable Key الذي يبدأ بـ sb_publishable_',
+  SUPABASE_ANON_KEY: 'الصق_هنا_المفتاح_الحقيقي',
   APP_NAME: 'دليل كفر الزيات و القري المجاوره',
   ADMIN_WHATSAPP: '01016264637',
   VODAFONE_CASH: '01016264637'
